f8a744c974276aa85c728d972782c35851d256cc
bsow99 <44095111+bsow99@users.noreply.github.com>
2022-04-24 11:26:17 +0200

    invitations et google login

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ class Config(object):
     SECRET_KEY = binascii.hexlify(os.urandom(24))
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'users.db') # or 'sqlite:/// + global path to db 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    os.environ['OAUTHLIB_INSECURE_TRANSPORT'] = '1'
